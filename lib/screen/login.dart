98372c3bd46a3a908a3a9f7f880caa00524abbb0
NTH3C <147485340+NTH3C@users.noreply.github.com>
2023-12-20 15:19:18 +0100

    textfield + button V3

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -6,22 +6,19 @@ class Login extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: const Text('Log in')),
       backgroundColor: const Color.fromRGBO(117, 123, 200, 1),
       body: SafeArea(
           child: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            const Text(
-              'Log in',
-              style: TextStyle(
-                color: Color.fromARGB(255, 255, 255, 255),
-                fontSize: 40,
-              ),
+            const SizedBox(
+              height: 10,
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const Text('log in to your account',
+            const Padding(
+              padding: EdgeInsets.only(bottom: 64.0),
+              child: Text('log in to your account',
                   style: TextStyle(
                     color: Color.fromARGB(255, 255, 255, 255),
                     fontSize: 20,
@@ -29,40 +26,66 @@ class Login extends StatelessWidget {
                   )),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    hintText: 'Email'),
+              padding: const EdgeInsets.all(16.0),
+              child: SizedBox(
+                width: 250,
+                height: 60,
+                child: TextField(
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(color: Colors.white),
+                      ),
+                      hintText: 'Email'),
+                ),
               ),
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(40),
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    hintText: 'Password'),
+              padding: const EdgeInsets.only(top: 24.0),
+              child: SizedBox(
+                width: 250,
+                height: 60,
+                child: TextField(
+                  style:
+                      const TextStyle(color: Color.fromARGB(255, 255, 255, 0)),
+                  decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                        borderSide: const BorderSide(color: Colors.white),
+                      ),
+                      hintText: 'Password'),
+                ),
               ),
             ),
-            ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.white,
-                ),
-                onPressed: () {},
-                child: const Text(
-                  "Log in",
+            const Padding(
+              padding: EdgeInsets.only(top: 4.0),
+              child: Text('forgot your password ?',
                   style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                      letterSpacing: 0.8),
-                ))
+                    color: Color.fromARGB(255, 255, 255, 255),
+                    fontSize: 10,
+                    fontWeight: FontWeight.w200,
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 48.0),
+              child: SizedBox(
+                width: 250,
+                height: 60,
+                child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Log in",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                          letterSpacing: 0.8),
+                    )),
+              ),
+            )
           ],
         ),
       )),
