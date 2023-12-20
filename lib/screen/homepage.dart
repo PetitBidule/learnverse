8a55876d8bc8de32c53661ae3bcbd0061c05b689
PetitBidule <theo.saint-amand@orange.fr>
2023-12-20 11:30:30 +0100

    homeScreen

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -5,8 +5,43 @@ class ScreenLog extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(child: Text("sefh")),
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(117, 123, 200, 1),
+      body: SafeArea(
+          child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset("asset/image/logoLearnVerse.png"),
+            const Text(
+              'LearnVerse',
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 255, 255),
+                  fontSize: 40,
+                  fontWeight: FontWeight.w700),
+            ),
+            TextButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                    padding: MaterialStateProperty.all<EdgeInsets>(
+                        const EdgeInsets.symmetric(
+                            horizontal: 80, vertical: 15)),
+                    shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            side: const BorderSide(
+                                color: Color.fromARGB(255, 255, 255, 255))))),
+                child: const Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 0.8),
+                ))
+          ],
+        ),
+      )),
     );
   }
 }
