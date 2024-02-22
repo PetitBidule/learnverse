c5c1532196f6fa0532d09ec231889af71452fb66
NTH3C <147485340+NTH3C@users.noreply.github.com>
2024-02-22 11:47:04 +0100

    privacy policy

---
+++ lib/view/privacyPolicy.dart
@@ -0,0 +1,128 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:learnverse/utils/constants.dart';
+import 'package:learnverse/view/homeTheme_view.dart';
+import 'package:learnverse/widgets/square_background.dart';
+
+class privacypolicy extends StatefulWidget {
+  const privacypolicy({super.key});
+
+  @override
+  State<privacypolicy> createState() => _privacypolicyState();
+}
+
+class _privacypolicyState extends State<privacypolicy> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomLeft,
+                colors: [
+                  Color.fromRGBO(224, 195, 252, 1),
+                  Color.fromRGBO(203, 178, 254, 1),
+                  Color.fromRGBO(159, 160, 255, 1),
+                  AllConstants.backgroundContainer,
+                ],
+              ),
+            ),
+          ),
+          const SquareBackground(
+            right: 100,
+            top: 100,
+            backgroundSquare: Color.fromRGBO(142, 148, 242, 1),
+          ),
+          const SquareBackground(
+            right: 75,
+            top: 350,
+            backgroundSquare: Color.fromRGBO(117, 123, 233, 1),
+          ),
+          const SquareBackground(
+            left: 50,
+            top: 350,
+            backgroundSquare: Color.fromRGBO(218, 182, 252, 1),
+          ),
+          const SquareBackground(
+            left: 125,
+            bottom: 50,
+            backgroundSquare: Color.fromRGBO(203, 178, 254, 1),
+          ),
+          const SquareBackground(
+            right: 100,
+            bottom: 250,
+            backgroundSquare: Color.fromRGBO(224, 195, 252, 1),
+          ),
+          BackdropFilter(
+            filter: ImageFilter.blur(sigmaX: 50.0, sigmaY: 50.0),
+            child: Container(
+              color: Colors.black.withOpacity(0.1),
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: SingleChildScrollView(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.arrow_back),
+                      color: Colors.white,
+                      onPressed: () => setState(() => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ThemeScreen()),
+                          )),
+                      iconSize: 36.0,
+                    ),
+                    const Text(
+                      'Privacy Policy',
+                      style: TextStyle(
+                        fontSize: 36.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.indigoAccent,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    const Divider(),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      '''Learnverse built the LearnVerse app as a Freemium app. This SERVICE is provided by Learnverse at no cost and is intended for use as is.
+
+This page is used to inform visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service.
+
+If you choose to use our Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that we collect is used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.
+
+The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at LearnVerse unless otherwise defined in this Privacy Policy.''',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      '''For a better experience, while using our Service, we may require you to provide us with certain personally identifiable information, including but not limited to Email, username. The information that we request will be retained by us and used as described in this privacy policy.
+
+The app does use third-party services that may collect information used to identify you.
+
+Link to the privacy policy of third-party service providers used by the app:
+
+* [Google Play Services](https://www.google.com/policies/privacy/)
+* [AdMob](https://support.google.com/admob/answer/6128543?hl=en)''',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
