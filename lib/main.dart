3d9314889b07b65db3b4b5a77ab83bc7b63fdf5f
aurel95 <aurelien.traini@gmail.com>
2024-04-02 10:51:25 +0200

    language page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:learnverse/Model/dbHelper/mongo_db.dart';
-import 'package:learnverse/view/chooseTheme_view.dart';
-import 'package:learnverse/view/dashboard_view.dart';
 import 'package:learnverse/view/homepage_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:learnverse/view/language.dart';
@@ -27,7 +25,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const MaterialApp(
+    return const MaterialApp(
       // theme: ThemeData(
       //     elevatedButtonTheme:  ElevatedButtonThemeData(
       //         style: ButtonStyle(backgroundColor: MaterialStateProperty<Color> Colors.white))),
